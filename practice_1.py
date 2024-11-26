1752123b79565207ede5974cacd21d022724b8e4
Zuzip05 <pechovazuzana@seznam.cz>
2024-11-26 22:39:47 +0100

    newcode

---
+++ practice_1.py
@@ -0,0 +1,11 @@
+#Prvních 5 písmen slova "indexování"
+print("Prvních 5 písmen:" )
+print("indexování"[:5])
+
+#Posledních 5 písmen slova "indexování"
+print("Posledních 5 písmen:")
+print("indexování"[-5:])
+
+#Každé 3. písmeno (počínaje prvním) od 'i':
+print("Každé 3. písmeno (počínaje prvním) od 'i':")
+print ("indexování"[::3])
